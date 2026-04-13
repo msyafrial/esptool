55004bdea45a2dd3c616b920f058b689f8894629
msyafrial <82314708+msyafrial@users.noreply.github.com>
2026-04-13 20:44:20 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/esptools/',
   plugins: [vue()],
   resolve: {
     alias: {
