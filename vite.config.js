dcf76e09101d98891ccb5d84c4b3a1f30ddff55d
msyafrial <82314708+msyafrial@users.noreply.github.com>
2026-04-13 21:39:37 +0700

    v1.0.0

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
-  base: '/esptool/',
+  base: './esptool/',
   plugins: [
     vue(),
     vuetify({
