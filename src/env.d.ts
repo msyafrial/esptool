4c5319d90664b4b49361a1dc3655339f66f9c7e3
msyafrial <82314708+msyafrial@users.noreply.github.com>
2026-04-13 21:21:59 +0700

    upload ulang

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_E2E?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
